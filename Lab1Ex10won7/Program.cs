3dc0a79c02fd2f746c5000dd52a008d5c703f3c5
Cristiano Py <Cristiano Py@DESKTOP-U2EFTL3>
2024-02-22 10:58:24 +0200

    Add project files.

---
+++ Lab1Ex10won7/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Scrieti un program care sa va afisa suma cifrelor ununi numar intreg citit de la
+tastatura.*/
+
+int sum = 0;
+int mod;    
+
+Console.WriteLine("Introduce un numar: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+
+while (num > 0)
+{
+    mod = num % 10;
+    sum += mod;
+    num /= 10;
+}
+
+Console.WriteLine("Suma este: " +  sum);
+
+Console.ReadLine();
